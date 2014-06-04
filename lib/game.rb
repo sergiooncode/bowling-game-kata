4ef042fa646e2a9ac7d4bc1f2864df6d7cc8b2d6
Sergio Perez <sperez4mba@gmail.com>
2014-06-04 13:59:43 -0500

    Refactored to score spare game right.

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,31 @@
 class Game
-  attr_reader :score
+  attr_reader :rolls, :current_roll
 
   def initialize
-    @score = 0
+    @rolls = Array.new(21)
+    (0..20).each do |i|
+      @rolls[i] = 0
+    end
+    @current_roll = 0
   end
 
   def roll(pins)
-    @score += pins
+    @rolls[@current_roll] = pins
+    @current_roll += 1
   end
+
   def score
-    return @score
+    score = 0
+    frame_index = 0
+    (0..9).each do |frame|
+      if @rolls[frame_index] + @rolls[frame_index + 1] == 10
+        score += 10 + @rolls[frame_index + 2]
+        frame_index += 2
+      else
+        score += @rolls[frame_index] + @rolls[frame_index + 1]
+        frame_index += 2
+      end
+    end
+    score
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,7 +14,6 @@ describe Game do
   end
   it "tests all ones game" do
     roll_many(20, 1)
-
     expect(game.score).to eql 20
   end
   it "tests one spare game" do
@@ -22,7 +21,6 @@ describe Game do
     game.roll(5)
     game.roll(3)
     roll_many(17, 0)
-
     expect(game.score).to eql 16
   end
 end
