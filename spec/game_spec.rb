c20fe2ae6201ea38ab34b0f07108237cc511e572
Sergio Perez <sperez4mba@gmail.com>
2014-06-04 11:39:29 -0500

    Refactoring.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,18 @@ require 'game'
 
 describe Game do
   let(:game){ described_class.new }
+  
+  def roll_many(n, pins)
+    n.times { game.roll(pins) }
+  end
+  
   it "tests gutter game" do
-    20.times { game.roll(0) }
+    roll_many(20, 0)  
 
     expect(game.score).to eql 0
   end
   it "tests all ones game" do
-    20.times { game.roll(1) }
+    roll_many(20, 1)
 
     expect(game.score).to eql 20
   end
