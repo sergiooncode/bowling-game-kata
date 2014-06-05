62ff0169282988efcd7d0581b2ead0613711c476
Sergio Perez <sperez4mba@gmail.com>
2014-06-05 12:21:05 -0500

    Refactoring.

--- lib/game.rb
+++ lib/game.rb
@@ -19,19 +19,31 @@ class Game
     frame_index = 0
     (0..9).each do |frame|
       if @rolls[frame_index] == 10
-        score += 10 + @rolls[frame_index + 1] + @rolls[frame_index + 2]
+        score += 10 + strike_bonus(frame_index)
         frame_index += 1
       else
         if spare?(frame_index)
-          score += 10 + @rolls[frame_index + 2]
+          score += 10 + spare_bonus(frame_index)
           frame_index += 2
         else
-          score += @rolls[frame_index] + @rolls[frame_index + 1]
+          score += sum_of_balls_in_frame(frame_index)
           frame_index += 2
         end
       end
     end
     score
+  end
+
+  def sum_of_balls_in_frame(frame_index)
+    @rolls[frame_index] + @rolls[frame_index + 1]
+  end
+
+  def spare_bonus(frame_index)
+    @rolls[frame_index + 2]
+  end
+
+  def strike_bonus(frame_index)
+    @rolls[frame_index + 1] + @rolls[frame_index + 2]
   end
 
   def spare?(frame_index) 
